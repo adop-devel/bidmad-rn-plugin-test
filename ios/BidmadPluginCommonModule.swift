1c681e18f6a9510c380c2a314c45af7f97c2ea84
MarkusOhADOP <83572705+MarkusOhADOP@users.noreply.github.com>
2023-12-06 14:28:13 +0900

    fix: ios-async-support-for-common

--- ios/BidmadPluginCommonModule.swift
+++ ios/BidmadPluginCommonModule.swift
@@ -39,19 +39,21 @@ class BidmadPluginCommonModule: NSObject, RCTBridgeModule {
         }
     }
     
-    func setAdvertiserTracking(enable: Bool) {
+    func setAdvertiserTracking(enable: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setAdvertiserTrackingEnabled(enable)
+        resolve(nil);
     }
     
-    func getAdvertiserTracking(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func getAdvertiserTracking(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         resolve(BidmadCommon.getAdvertiserTrackingEnabled())
     }
     
-    func set(isChildDirectedAds: Bool) {
+    func set(isChildDirectedAds: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setIsChildDirectedAds(isChildDirectedAds)
+        resolve(nil);
     }
     
-    func isChildDirectedTreatment(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func isChildDirectedTreatment(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         guard let isChildDirectedAds = BidmadCommon.isChildDirectedTreatment() else {
             resolve(nil)
             return
@@ -60,11 +62,12 @@ class BidmadPluginCommonModule: NSObject, RCTBridgeModule {
         resolve(isChildDirectedAds.boolValue)
     }
     
-    func set(userConsentStatusForCCPACompliance: Bool) {
+    func set(userConsentStatusForCCPACompliance: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setUserConsentStatusForCCPACompliance(userConsentStatusForCCPACompliance)
+        resolve(nil)
     }
     
-    func isUserConsentCCPA(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func isUserConsentCCPA(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         guard let isUserConsentCCPA = BidmadCommon.isUserConsentCCPA() else {
             resolve(nil)
             return
@@ -73,35 +76,39 @@ class BidmadPluginCommonModule: NSObject, RCTBridgeModule {
         resolve(isUserConsentCCPA.boolValue)
     }
     
-    func set(isDebug: Bool) {
+    func set(isDebug: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setIsDebug(isDebug)
+        resolve(nil)
     }
     
-    func isDebug(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func isDebug(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         resolve(BidmadCommon.isDebug())
     }
     
-    func set(testDeviceId: String) {
+    func set(testDeviceId: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setTestDeviceId(testDeviceId)
+        resolve(nil)
     }
     
-    func testDeviceId(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func testDeviceId(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         resolve(BidmadCommon.testDeviceId())
     }
 
-    func set(cuid: String) {
+    func set(cuid: String, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setCuid(cuid)
+        resolve(nil)
     }
     
-    func cuid(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func cuid(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         resolve(BidmadCommon.cuid())
     }
     
-    func set(useServerSideCallback: Bool) {
+    func set(useServerSideCallback: Bool, resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         BidmadCommon.setUseServerSideCallback(useServerSideCallback)
+        resolve(nil)
     }
     
-    func useServerSideCallback(resolver resolve: @escaping RCTPromiseResolveBlock, rejecter reject: @escaping RCTPromiseRejectBlock) {
+    func useServerSideCallback(resolver resolve: RCTPromiseResolveBlock, rejecter reject: RCTPromiseRejectBlock) {
         resolve(BidmadCommon.useServerSideCallback())
     }
 }
